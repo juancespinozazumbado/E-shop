30994f8d214673313395117a293e73534b5049e7
Juan Carlos Espinoza Zumbado <juan.espinozazumbado@ucr.ac.cr>
2023-05-30 12:09:52 -0600

    first spring

---
+++ src/main/java/com/eshop/shop/ShopApplication.java
@@ -0,0 +1,28 @@
+package com.eshop.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ShopApplication {
+
+	public static void main(String[] args)
+	{
+
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+	@GetMapping("/hellow")
+	public static String hellow(@RequestParam(value = "myName",
+			defaultValue = "World") String name){
+		return "Hellow"+" "+ name;
+
+	}
+
+
+
+}
---
+++ src/test/java/com/eshop/shop/ShopApplicationTests.java
@@ -0,0 +1,15 @@
+package com.eshop.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}
+
+	 */
+
+}
